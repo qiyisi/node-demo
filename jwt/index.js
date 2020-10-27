e3d63fb1cb3685f7c1337d1bd88521042b5addaa
qiyisi <qiyisi@qq.com>
2020-10-27 22:38:28 +0800

    add jwt

---
+++ jwt/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+// const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+
+// Import Routes
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+// Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => console.log("connected to db!")
+);
+
+// Middleware
+app.use(express.json());
+// app.use(bodyParser.json());
+// Route Middlewares
+app.use("/api/user", authRoute);
+
+app.listen(3000, () => console.log("Server up and running"));
