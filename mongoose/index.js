0710f1350e7360bf2df818b76576fe9703860a3f
qiyisi <qiyisi@qq.com>
2020-10-28 12:27:15 +0800

    add mongoose

---
+++ mongoose/index.js
@@ -0,0 +1,32 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/test");
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("we're connected!");
+});
+
+var kittySchema = mongoose.Schema({
+  name: String,
+});
+
+kittySchema.methods.speak = function () {
+  var greeting = this.name
+    ? "Meow name is " + this.name
+    : "I don't have a name";
+  console.log(greeting);
+};
+
+var Kitten = mongoose.model("Kitten", kittySchema);
+
+var fluffy = new Kitten({ name: "fluffy" });
+
+fluffy.save(function (err, fluffy) {
+  if (err) return console.error(err);
+  fluffy.speak();
+});
+
+Kitten.find(function (err, kittens) {
+  if (err) return console.error(err);
+  console.log(kittens);
+});
